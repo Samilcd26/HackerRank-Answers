dc523ed7cf2e4e9d18ff1ca8659c180a01c5f566
Samilcd26 <83214912+Samilcd26@users.noreply.github.com>
2023-01-09 20:17:55 +0300

    Two questions solved

---
+++ HackerRank/ExtraLongFactorials.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public class ExtraLongFactorials
+    {
+        public static void extraLongFactorials(int n)
+        {
+            if (n == 0) Console.WriteLine(0);
+
+            BigInteger total = new BigInteger(1);
+            for (int i = n; 0 < i; i--)
+            {
+                total *= i;
+            }
+
+            Console.WriteLine(total.ToString());
+        }
+    }
+}
---
+++ HackerRank/Staircase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public class Staircase
+    {
+        public static void staircase(int n)
+        {
+            string Out = "";
+            for (int i = 1; i <= n; i++)
+            {
+
+                Console.WriteLine(Out.PadLeft((i), '#').PadLeft((n), ' '));
+            }
+        }
+    }
+}
