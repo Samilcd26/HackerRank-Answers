442fa7d65bf400e7b364e50104d871f3b20c894f
Samilcd26 <83214912+Samilcd26@users.noreply.github.com>
2023-01-10 23:43:24 +0300

    2 question solved

---
+++ HackerRank/MiniMaxSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public class MiniMaxSum
+    {
+        public static void miniMaxSum(List<int> arr)
+        {
+            BigInteger min = 0, max = 0;
+            arr.Sort();
+            for (int i = 0; i < arr.Count - 1; i++)
+            {
+
+                min += arr[i];
+            }
+            arr.Reverse();
+            for (int j = 0; j < arr.Count - 1; j++)
+            {
+                max += arr[j];
+            }
+
+            Console.WriteLine(min + " " + max);
+        }
+    }
+}
---
+++ HackerRank/StringToList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public class StringToList
+    {
+        public List<int> stringToList(string str) {
+
+           
+            string[] words = str.Split(' ');
+            List<int> list = new List<int>();
+
+            foreach (var word in words)
+            {
+                list.Add(Int32.Parse(word));
+                
+            }
+
+            return list;
+        }
+
+    }
+}
