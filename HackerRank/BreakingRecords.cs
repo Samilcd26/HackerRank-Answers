73c009edb1ecba47cca80d5ee8490055080fc87c
Samilcd26 <83214912+Samilcd26@users.noreply.github.com>
2023-01-11 23:54:30 +0300

    one solve

---
+++ HackerRank/BreakingRecords.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public class BreakingRecords
+    {
+        public static List<int> breakingRecords(List<int> scores)
+        {
+            List<int> respon = new List<int>();
+            int minScor = 0, maxScor = 0;
+            int Min = scores[0];
+            int Max = scores[0];
+
+            foreach (int x in scores)
+            {
+                if (x < Min && x > Max)
+                {
+                    Min = x;
+                    Max = x;
+                    minScor += 1;
+                    maxScor += 1;
+
+                }
+                else if (x > Max)
+                {
+
+                    Max = x;
+                    maxScor += 1;
+                }
+                else if (x < Min)
+                {
+
+                    Min = x;
+                    minScor += 1;
+                }
+            };
+            respon.Add(maxScor);
+            respon.Add(minScor);
+
+
+            return respon;
+        }
+    }
+}
---
+++ HackerRank/CountApplesAndOranges.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public class CountApplesAndOranges
+    {
+        public static void countApplesAndOranges(int s, int t, int a, int b, List<int> apples, List<int> oranges)
+        {
+            int appleLog = 0;
+            int orangeLog = 0;
+
+            foreach (int i in apples)
+            {
+                if (s <= (a + i) && (a + i) <= t)
+                {
+                    appleLog += 1;
+                }
+            }
+
+            foreach (int o in oranges)
+            {
+                if (s <= (b + o) && (b + o) <= t)
+                {
+                    orangeLog += 1;
+                }
+            }
+
+            Console.WriteLine(appleLog);
+            Console.WriteLine(orangeLog);
+
+        }
+    }
+}
